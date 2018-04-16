a6cb821d6eb825b07d72f33070f4a6ecab2eb90b
Judson <nyarly@gmail.com>
2018-04-16 09:20:10 -0700

    Adding apache to shell.nix

--- shell.nix
+++ shell.nix
@@ -2,6 +2,6 @@
 with pkgs;
 stdenv.mkDerivation {
   name = "d2tools";
-  buildInputs = [ openssl zlib sqlite sqlite-interactive ];
+  buildInputs = [ openssl zlib sqlite sqlite-interactive apacheHttpd ];
   nativeBuildInputs = [ pkgconfig ];
 }
